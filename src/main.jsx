b06df6c38f3aa2a03b914b354f67445a997391f1
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-12-05 14:15:27 +1000

    Task completed!!!

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-import { Layout } from './components/Layout/Layout.jsx'
+import { App } from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<Layout />
+		<App />
 	</Provider>
 )
